6ed59a3d8852987fae80e7d01b644d5e9350de22
laxxxx <lakshmancool9@gmail.com>
2024-11-14 16:09:06 +0530

    first commit

---
+++ okulrAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  okulrAssignment
+//
+//  Created by Sree Lakshman on 14/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
